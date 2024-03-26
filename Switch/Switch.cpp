daefb690584b0c993d073ab9d1e18ee8a14febd1
syairaarif <putrisyafira4444@gmail.com>
2024-03-26 11:02:28 +0700

    membuat switch case dengan perulangan do while

--- Switch/Switch.cpp
+++ Switch/Switch.cpp
@@ -32,7 +32,53 @@ void inputdata()
     cout << "Masukan bilangan kedua : ";
     cin >> bilangan2;
 }
+
 int main()
 {
+    int pilihan;
+    do
+    {
+        system("CLS");
+        cout << "menu kalkulator sederhana" << endl;
+        cout << "1. penjumlahan" << endl;
+        cout << "2. pengurangan" << endl;
+        cout << "3. perkalian" << endl;
+        cout << "4. pembagian" << endl;
+        cout << "5. keluar" << endl;
+        cout << "masukan pilihan : ";
+        cin >> pilihan;
+
+        switch(pilihan)
+        {
+        case 1:
+            inputdata();
+            cout << "\nHasil penjumlahan : " << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 2:
+            inputdata();
+            cout << "\nHasil pengurangan : " << pengurangan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 3:
+            inputdata();
+            cout << "\nHasil perkalian : " << perkalian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 4:
+            inputdata();
+            cout << "\nHasil pembagian : " << pembagian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 5:
+            break;
+        default:
+            cout << "pilihan tidak valid" << endl;
+            system("pause");
+            break;
+        }
+    } while (pilihan != 5);
+
+    return 0;
     
 }
